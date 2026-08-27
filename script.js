12db149e58dd107089d49759764eb271bf377557
Chaitra Nadagoudra <chaitranadagoudra4@gmail.com>
2026-08-27 10:02:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//46. Write JavaScript code to print Hello World using console.log().
+console.log("Hello World");
+
+//47. Write JavaScript code to print the number 500 using console.log().
+console.log(500);
+
+//48. What is the purpose of console.warn()?
+console.warn("This is a warning");
+
+//49. What is the purpose of console.error()?
+//---this is used to display an error message in the browser console
+console.error("This is an error");
+
+//50. What is the purpose of each?
+//1. alert()
+alert("Hello World");
+//2. prompt()
+prompt("Enter your name");
+//3. confirm()
+confirm("Are you sure?");
+//4. document.writeln()
+document.writeln("Hello World");
+//5. console.log()
+console.log("Hello World");
+
+//1)
+let studentName = "Chaitra";
+let age = 23;
+let mark = 85;
+
+console.log(studentName);
+console.log(age);
+console.log(mark);
+
+//2)
+let name = prompt("Enter your name");
+alert(name);
+
+//3)
+let AGE = prompt("Enter your age");
+console.log(age);
+
+//4)
+let answer = confirm("Are you ready to learn JavaScript?");
+console.log(answer);
+
+//5)
+let ANSWER = confirm("Are you ready to learn JavaScript?");
+console.log(answer);
+      
+    
